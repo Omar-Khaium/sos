52bbe896a05847faefb06ef6f5d0e44609bc2063
Omar <omar-khaium>
2021-03-10 23:36:48 +0600

    init

---
+++ lib/src/provider/provider_theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ThemeProvider extends ChangeNotifier {
+  Color accentColor = Colors.indigo;
+  Color textColor = Colors.grey.shade800;
+  Color iconColor = Colors.grey.shade700;
+  Color shadowColor = Colors.black12;
+  Color backgroundColor = Colors.white;
+  Color secondaryColor = Colors.grey.shade50;
+}
